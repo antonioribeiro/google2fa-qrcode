eeaf845dfeb392c0e66bca7f75bc635ebdf73f69
ant <ant@antsplace.co.uk>
2021-05-12 21:43:15 +0100

    Fixed for use with PHP8

--- src/Google2FA.php
+++ src/Google2FA.php
@@ -2,38 +2,29 @@
 
 namespace PragmaRX\Google2FAQRCode;
 
-use BaconQrCode\Writer;
-use BaconQrCode\Renderer\Image\Png;
-use BaconQrCode\Renderer\ImageRenderer;
-use PragmaRX\Google2FAQRCode\QRCode\Bacon;
-use PragmaRX\Google2FAQRCode\QRCode\Chillerlan;
-use BaconQrCode\Renderer\Image\RendererInterface;
-use BaconQrCode\Writer as BaconQrCodeWriter;
-use BaconQrCode\Renderer\Image\ImagickImageBackEnd;
 use PragmaRX\Google2FA\Google2FA as Google2FAPackage;
-use BaconQrCode\Renderer\RendererStyle\RendererStyle;
+use BaconQrCode\Renderer\ImageRenderer;
 use BaconQrCode\Renderer\Image\ImageBackEndInterface;
-use PragmaRX\Google2FAQRCode\Exceptions\MissingQrCodeServiceException;
+use BaconQrCode\Renderer\Image\SvgImageBackEnd;
+use BaconQrCode\Renderer\RendererStyle\RendererStyle;
+use BaconQrCode\Writer;
 
 class Google2FA extends Google2FAPackage
 {
     /**
      * @var ImageBackEndInterface|RendererInterface|null $imageBackEnd
      */
-    protected $qrCodeService;
+    protected $imageBackEnd;
 
-    /**
-     * Google2FA constructor.
-     *
-     * @param ImageBackEndInterface|RendererInterface|null $imageBackEnd
-     */
-    public function __construct($qrCodeService = null)
+    public function __construct($imageBackEnd = null)
     {
-        $this->setQrCodeService(
-            empty($qrCodeService)
-                ? $this->qrCodeServiceFactory()
-                : $qrCodeService
-        );
+
+        if ($imageBackEnd instanceof ImageBackEndInterface) {
+            $this->imageBackEnd = $imageBackEnd;
+        } else {
+            $this->imageBackEnd = new SvgImageBackEnd();
+        }
+
     }
 
     /**
@@ -47,69 +38,39 @@ class Google2FA extends Google2FAPackage
      *
      * @return string
      */
-    public function getQRCodeInline(
-        $company,
-        $holder,
-        $secret,
-        $size = 200,
-        $encoding = 'utf-8'
-    ) {
-        if (empty($this->getQrCodeService())) {
-            throw new MissingQrCodeServiceException(
-                'You need to install a service package or assign yourself the service to be used.'
-            );
-        }
+    public function getQRCodeInline($company, $holder, $secret, $size = 200, $encoding = 'utf-8')
+    {
+        return $this->getQRCodeInlineV2($company, $holder, $secret, $size, $encoding);
+    }
 
-        return $this->qrCodeService->getQRCodeInline(
+    /**
+     * Generates a QR code data url to display inline for Bacon QRCode v2
+     *
+     * @param string $company
+     * @param string $holder
+     * @param string $secret
+     * @param int    $size
+     * @param string $encoding Default to UTF-8
+     *
+     * @return string
+     */
+    public function getQRCodeInlineV2($company, $holder, $secret, $size = 200, $encoding = 'utf-8')
+    {
+        $renderer = new ImageRenderer(
+            (new RendererStyle($size))->withSize($size),
+            $this->imageBackEnd
+        );
+
+        $bacon = new Writer($renderer);
+
+        $data = $bacon->writeString(
             $this->getQRCodeUrl($company, $holder, $secret),
-            $size,
             $encoding
         );
-    }
 
-    /**
-     * Service setter
-     *
-     * @return \PragmaRX\Google2FAQRCode\QRCode\QRCodeServiceContract
-     */
-    public function getQrCodeService()
-    {
-        return $this->qrCodeService;
-    }
-
-    /**
-     * Service setter
-     *
-     * @return self
-     */
-    public function setQrCodeService($service)
-    {
-        $this->qrCodeService = $service;
-
-        return $this;
-    }
-
-    /**
-     * Create the QR Code service instance
-     *
-     * @return \PragmaRX\Google2FAQRCode\QRCode\QRCodeServiceContract
-     */
-    public function qrCodeServiceFactory()
-    {
-        if (
-            class_exists('BaconQrCode\Writer') ||
-            class_exists('BaconQrCode\Renderer\ImageRenderer')
-        ) {
-            return new Bacon();
+        if ($this->imageBackEnd instanceof SvgImageBackEnd) {
+            return 'data:image/svg+xml;base64,'.base64_encode($data);
         }
-
-        if (
-            class_exists('chillerlan\QRCode') ||
-            class_exists('BaconQrCode\Renderer\ImageRenderer')
-        ) {
-            return new Chillerlan();
-        }
-
-        return null;
+        return $data;
     }
 }
